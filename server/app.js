c1cc511308e27be6c886d7981ee5ca99fca0a9bf
Kenil Gopani <gopanikenil26@gmail.com>
2026-07-05 16:23:15 +0530

    Fix CORS by dynamically removing trailing slash from CLIENT_URL

--- server/app.js
+++ server/app.js
@@ -11,8 +11,9 @@ const app = express()
 app.use(helmet())
 
 // CORS
+const clientUrl = process.env.CLIENT_URL ? process.env.CLIENT_URL.replace(/\/$/, '') : 'http://localhost:5173'
 app.use(cors({
-  origin:      process.env.CLIENT_URL || 'http://localhost:5173',
+  origin:      clientUrl,
   credentials: true,
 }))
 
