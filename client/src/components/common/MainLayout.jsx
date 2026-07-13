95e4cf6c742e425573efdb38a8c8bfbefe50381d
Kenil Gopani <gopanikenil26@gmail.com>
2026-07-13 21:36:51 +0530

    feat: automatically close mobile sidebar on route change

--- client/src/components/common/MainLayout.jsx
+++ client/src/components/common/MainLayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function MainLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const location = useLocation()
+
+  // Automatically close sidebar on any route change (e.g. clicking a link, back button)
+  useEffect(() => {
+    setSidebarOpen(false)
+  }, [location.pathname])
 
   return (
     <div className="flex layout-height overflow-hidden">
